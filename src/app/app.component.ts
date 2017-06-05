8e3e25beb902045911b42dbe4d9e193268fc5e82
Bogdan Matragociu <matragoc@adobe.com>
2017-06-05 14:35:14 +0300

    Language EN/RO only.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -76,13 +76,14 @@ export class MyApp {
 
   initTranslate() {
     // Set the default language for translation strings, and the current language.
-    this.translate.setDefaultLang('en');
+    this.translate.setDefaultLang('ro');
 
-    if (this.translate.getBrowserLang() !== undefined) {
-      this.translate.use(this.translate.getBrowserLang());
-    } else {
-      this.translate.use('en'); // Set your language here
-    }
+    // TODO Default to browser language, use romanian for the moment
+    // if (this.translate.getBrowserLang() !== undefined) {
+    //   this.translate.use(this.translate.getBrowserLang());
+    // } else {
+      this.translate.use('ro'); // Set your language here
+    // }
 
     this.translate.get(['BACK_BUTTON_TEXT']).subscribe(values => {
       this.config.set('ios', 'backButtonText', values.BACK_BUTTON_TEXT);
--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -35,17 +35,7 @@ export class TutorialPage {
           {
             title: values.TUTORIAL_SLIDE1_TITLE,
             description: values.TUTORIAL_SLIDE1_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-1.png',
-          },
-          {
-            title: values.TUTORIAL_SLIDE2_TITLE,
-            description: values.TUTORIAL_SLIDE2_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-2.png',
-          },
-          {
-            title: values.TUTORIAL_SLIDE3_TITLE,
-            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-3.png',
+            image: 'assets/img/tooth.png',
           }
         ];
       });
